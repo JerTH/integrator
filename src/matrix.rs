eca040f13d4236003ce0f47cdb21c2c7a6dc018e
jerth <jeremyhatcher27@gmail.com>
2024-07-11 20:10:48 -0300

    0.1.36

--- src/matrix.rs
+++ src/matrix.rs
@@ -117,18 +117,7 @@ impl Matrix {
             },
         }
     }
-
-    fn convert_matrix(mat: &cgmath::Matrix4<Float>) -> Matrix {
-        Matrix {
-            elements: [
-                [mat[0][0], mat[0][1], mat[0][2], mat[0][3]],
-                [mat[1][0], mat[1][1], mat[1][2], mat[1][3]],
-                [mat[2][0], mat[2][1], mat[2][2], mat[2][3]],
-                [mat[3][0], mat[3][1], mat[3][2], mat[3][3]],
-            ],
-        }
-    }
-
+    
     /// Multiplies two matrices and returns the resulting [Matrix]
     /// 
     /// ```
